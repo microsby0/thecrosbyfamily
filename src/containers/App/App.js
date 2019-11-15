0cb944aa17557d457b8c2fa08930b1f3fa48cfd0
Michael Crosby <mxc5i2x@c02vpa1lhtd6mbp.amer.homedepot.com>
2019-11-15 10:27:03 -0500

    init

---
+++ src/containers/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>The Crosbys</h1>
+      </header>
+    </>
+  );
+}
+
+export default App;
