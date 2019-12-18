b56496946ca0fb692e70cbc7968629d8f2bdd4bd
microsby0 <microsby0@gmail.com>
2019-12-18 09:48:08 -0500

    move to typescript and change dir structure

--- scripts/install-and-activate-google-cloud-sdk.sh
+++ scripts/install-and-activate-google-cloud-sdk.sh
@@ -2,23 +2,22 @@
 
 set -e
 
-echo ${GCLOUD_SDK_URL}
-#GCLOUD_PROJECT=${GOOGLE_PROJECT_ID}
-## expecting the install directly in the home directory
-#GCLOUD=${HOME}/google-cloud-sdk/bin/gcloud
-#echo ${GCLOUD_SDK_URL}
-#if ${GCLOUD} version 2>&1 >> /dev/null; then
-#    echo "Cloud SDK is already installed"
-#else
-#    SDK_URL=${GCLOUD_SDK_URL}
-#    INSTALL_DIR=${HOME}
-#
-#    cd ${INSTALL_DIR}
-#    wget ${SDK_URL} -O cloud-sdk.tar.gz
-#    tar -xzvf cloud-sdk.tar.gz
-#
-#    ${GCLOUD} components install app-engine-java
-#fi
-#
-#echo ${GCLOUD_SERVICE_KEY} | ${GCLOUD} auth activate-service-account --key-file=-
-#${GCLOUD} config set project ${GCLOUD_PROJECT}
+GCLOUD_PROJECT=${GOOGLE_PROJECT_ID}
+# expecting the install directly in the home directory
+GCLOUD=${HOME}/google-cloud-sdk/bin/gcloud
+
+if ${GCLOUD} version 2>&1 >> /dev/null; then
+    echo "Cloud SDK is already installed"
+else
+    SDK_URL="https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-274.0.0-linux-x86_64.tar.gz"
+    INSTALL_DIR=${HOME}
+
+    cd ${INSTALL_DIR}
+    wget ${SDK_URL} -O cloud-sdk.tar.gz
+    tar -xzvf cloud-sdk.tar.gz
+
+    ${GCLOUD} components install app-engine-java
+fi
+
+echo ${GCLOUD_SERVICE_KEY} | ${GCLOUD} auth activate-service-account --key-file=-
+${GCLOUD} config set project ${GCLOUD_PROJECT}
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+const App: React.FC = () => (
+  <>
+    <header>
+      <h1>The Crosbys</h1>
+    </header>
+  </>
+);
+
+export default App;
